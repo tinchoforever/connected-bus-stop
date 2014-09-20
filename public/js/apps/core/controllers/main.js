c1fc8fbcd6b09e6582404f846f5bc080b96ad359
martin <martin@rga.com>
2014-09-20 16:04:35 -0300

    dates among other yerbas

--- public/js/apps/core/controllers/main.js
+++ public/js/apps/core/controllers/main.js
@@ -5,27 +5,36 @@ ff.controller('mainController', function ($scope,$location, $timeout, $http) {
     
     var bus = [];
 
-    
+	var historyMock60 = [];
+	for (var i = 4; i > 0; i--) {
+		historyMock60.push(moment(new Date()).add(-i, 'minutes'));
+	};  
+	var historyMock168 = [];
+	for (var i = 4; i > 0; i--) {
+		historyMock168.push(moment(new Date()).add(-i*i, 'minutes'));
+	};  
     var recent = [];
 
     bus.push({
     	id:60,
-    	timestamp: Date.parse('2014-09-20 14:22'), 
+    	timestamp: moment(new Date()).add(-Math.floor((Math.random() * 10) + 1), 'minutes'),
     	estimation: moment(new Date()).add(Math.floor((Math.random() * 10) + 1), 'minutes'),
+    	history:historyMock60
     });
     bus.push({
     	id:168,
-    	timestamp: Date.parse('2014-09-20 14:23'), 
+    	timestamp: moment(new Date()).add(-Math.floor((Math.random() * 10) + 1), 'minutes'),
     	estimation: moment(new Date()).add(Math.floor((Math.random() * 10) + 1), 'minutes'),
+    	history:historyMock168
     });
     bus.push({
     	id:152,
-		timestamp: Date.parse('2014-09-20 14:23'), 
+    	timestamp: moment(new Date()).add(-Math.floor((Math.random() * 10) + 1), 'minutes'),
     	estimation: moment(new Date()).add(Math.floor((Math.random() * 10) + 1), 'minutes'), 
     });
     bus.push({
     	id:71,
-    	timestamp: Date.parse('2014-09-20 14:23'), 
+    	timestamp: moment(new Date()).add(-Math.floor((Math.random() * 10) + 1), 'minutes'),
     	estimation: moment(new Date()).add(Math.floor((Math.random() * 10) + 1), 'minutes'),
     });
 
@@ -52,15 +61,44 @@ ff.controller('mainController', function ($scope,$location, $timeout, $http) {
 
 	$scope.getEstimate =function(b){
 		
-		// if (b.history){
-		// b.history.push(b);
-
-		// if (b.history.lenght > 5){
-		// 	b.history.shift();
-		// }
+		if (!b.history){
+			b.history=[]
+		}
+		b.history.push(b.timestamp);
 		
+		if (b.history.lenght > 5){
+			b.history.shift();
+		}
+		var results = [];
+		
+		for (var i = b.history.length-1; i > 0 ; i--) {
+			
+			if (i-1 >= 0){
+				var first = moment(b.history[i]);
+				var second =moment(b.history[i-1]);
+				var r = first.diff(second, 'minutes', true);
+				console.log(r);
+				results.push(r);
+			}
+		};
+		var average = 0;
 
-		return moment(new Date()).add(Math.floor((Math.random() * 10) + 1), 'minutes');
+		for (var i = 0; i < results.length; i++) {
+			average += results[i];
+		}
+		if (results.length > 0 ){
+
+			average = average /results.length;
+			console.log('average',average);
+		}
+		else {
+			result = moment(new Date()).add(Math.floor((Math.random() * 10) + 1), 'minutes');
+			console.log('result',result);
+		}
+
+		var result = moment(new Date()).add(average, 'minutes');
+
+		return result;
 		
 	}
 
